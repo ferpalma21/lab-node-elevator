da40ec6dcc731ef807d3c6e9ce10ca0871c88842
Daniel Fernando Palma Lopez <palma@Daniels-MBP.home>
2017-07-04 00:57:56 +0200

    1st iteration finished

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,30 @@
+/*jshint esversion:6*/
+
 class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = 'up';
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    let intervalId = setInterval(() => {
+      log();
+    },1*1000);
+  }
+
+  stop() {clearInterval(intervalId);}
+  update() {this.log();}
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+
+  log() {
+    console.log("Direction: " + this.direction + ' | Floor: ' + this.floor);
+  }
 }
 
 module.exports = Elevator;
